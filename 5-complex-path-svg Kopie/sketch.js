14ca095cca4777429c2eff257140549bf8fb6178
mrkesele <96123746+mrkesele@users.noreply.github.com>
2022-01-11 15:50:42 +0100

    Update sketch.js

--- 5-complex-path-svg Kopie/sketch.js
+++ 5-complex-path-svg Kopie/sketch.js
@@ -8,7 +8,6 @@ Matter.use('matter-wrap');
 let ball;
 let polygon;
 let ground;
-let boxImg;
 
 function setup() {
   createCanvas(2800, 720);
@@ -24,13 +23,13 @@ function setup() {
   //);
 
   // use svg file to create the corresponding polygon
-  polygon = new PolygonFromSVG(world,
+  strecke01 = new PolygonFromSVG(world,
     { x: 150, y: 400, fromFile: './element2.svg', scale: 1, color: 'red' },
     { isStatic: true, friction: 0.0 }
   );
 
-  polygonTwo = new PolygonFromSVG(world,
-    { x: 900, y: 520, fromFile: './element4.svg', scale: 1, color: 'white' },
+  strecke02 = new PolygonFromSVG(world,
+    { x: 800, y: 520, fromFile: './element4.svg', scale: 1, color: 'white' },
     { isStatic: true, friction: 0.0 }
   );
 
@@ -41,7 +40,7 @@ function setup() {
   };
   ball = new Ball(world,
     { x: 30, y: 50, r: 26, color: 'white' },
-    { friction: 0.0, plugin: { wrap: wrap } }
+    { friction: 0.25, plugin: { wrap: wrap } }
   );
   
   startpunkt = new Block(world,
@@ -49,10 +48,15 @@ function setup() {
     { isStatic: true, angle: PI * 0.1  }
   );
 
-  boxImg = loadImage('element4.png');
+  stein01 = new Block(world,
+    { x: 850, y: 510, w: 40, h: 40, color: 'grey' },
+    { isStatic: true, angle: 0  }
+  );
 
-  box = new SpriteBlock(world, { x: 900, y: 200, w: 64, h: 64, image: boxImg, isStatic: true});
-  
+  eis01 = new Block(world,
+    { x: 1000, y: 516, w: 200, h: 10, color: 'blue' },
+    { isStatic: true, angle: 0, friction: 1  }
+  );
 
   // setup mouse
   mouse = new Mouse(engine, canvas);
@@ -65,16 +69,22 @@ function draw() {
   background('black');
 
   startpunkt.draw();
-  polygon.draw();
-  polygonTwo.draw();
+  strecke01.draw();
+  strecke02.draw();
   ball.draw();
   mouse.draw();
-  box.draw();
+  stein01.draw();
+  eis01.draw();
    // follow the ball by scrolling the window
 scrollFollow(ball);
-  
+
+//eis(ball);
+gameover(ball);
+
 }
 
+
+//Kamera
 function scrollFollow(object) {
   if (insideViewport(object) == false) {
     const $element = $('html, body');
@@ -100,19 +110,45 @@ function insideViewport(object) {
   }
 }
 
+function eis(object) {
+  const x = object.body.position.x;
+  const y = object.body.position.y;
+  if (x <= 1150) {
+    //object.body.position.x = object.body.position.x +1;
+  }
+  if (x <= 1150) {
+    //object.body.position.x = object.body.position.x +1;
+  }
+  else 
+  {
 
+  }
+}
+
+function gameover(object) {
+  const y = object.body.position.y;
+  if (y >= 720) {
+    Matter.Body.setPosition(object.body, {x:30,y:50});
+  }
+  else 
+  {
+
+  }
+}
+
+//Jump
 function keyPressed() {
   // is SPACE pressed?
   if (keyCode === 32) {
     let direction = 1; // ball runs left to right ->
-    if ((ball.body.position.x - ball.body.positionPrev.x) < 0) {
-      direction = -1; // ball runs right to left <-
-    }
-    // use current direction and velocity for the jump
+    //if ((ball.body.position.x - ball.body.positionPrev.x) < 0) {
+    //  direction = -1; // ball runs right to left <-
+    //}
+    // use current direction for the jump
     Matter.Body.applyForce(
       ball.body,
       {x: ball.body.position.x, y: ball.body.position.y},
-      {x: (0.001 * direction) + ball.body.velocity.x / 100, y: -0.07}
+      {x: (0.01 * direction) , y: -0.07}
     );
   }
 }
