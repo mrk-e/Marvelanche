79fd3eb1de66b46469855a6f251282eca47775a1
mrkesele <96123746+mrkesele@users.noreply.github.com>
2022-01-12 14:58:18 +0100

    Update sketch.js

--- 5-complex-path-svg Kopie/sketch.js
+++ 5-complex-path-svg Kopie/sketch.js
@@ -12,17 +12,16 @@ let ground;
 function setup() {
   createCanvas(2800, 720);
 
-  // create an engine
+  //create an engine
   const engine = Matter.Engine.create();
   const world = engine.world;
 
-  // use svg file to create the corresponding polygon
-  //polygon = new PolygonFromSVG(world,
-  //  { x: 145, y: 400, fromFile: './path.svg', scale: 0.8, color: 'white' },
-  //  { isStatic: true, friction: 0.0 }
-  //);
+  //Strecke und Böden
+  startpunkt = new Block(world,
+    { x: 0, y: 62, w: 100, h: 25, color: 'grey' },
+    { isStatic: true, angle: PI * 0.1  }
+  );
 
-  // use svg file to create the corresponding polygon
   strecke01 = new PolygonFromSVG(world,
     { x: 150, y: 400, fromFile: './element2.svg', scale: 1, color: 'red' },
     { isStatic: true, friction: 0.0 }
@@ -33,21 +32,26 @@ function setup() {
     { isStatic: true, friction: 0.0 }
   );
 
-  // ball and ground
+  bodenTest = new Block(world,
+    { x: 1500, y: 520, w: 800, h: 10, color: 'green' },
+    { isStatic: true, angle: 0, friction: 1  }
+  );
+
+
+  // Ball und Maus
   const wrap = {
     min: { x: 0, y: 0 },
     max: { x: width, y: height }
   };
+
+  mouse = new Mouse(engine, canvas);
+
   ball = new Ball(world,
     { x: 30, y: 50, r: 26, color: 'white' },
     { friction: 0.25, plugin: { wrap: wrap } }
   );
   
-  startpunkt = new Block(world,
-    { x: 0, y: 122, w: 100, h: 25, color: 'grey' },
-    { isStatic: true, angle: PI * 0.1  }
-  );
-
+  //Hindernisse und Spielmechanismen
   stein01 = new Block(world,
     { x: 850, y: 510, w: 40, h: 40, color: 'grey' },
     { isStatic: true, angle: 0  }
@@ -58,31 +62,33 @@ function setup() {
     { isStatic: true, angle: 0, friction: 1  }
   );
 
-  // setup mouse
-  mouse = new Mouse(engine, canvas);
-
-  // run the engine
+  //run the engine
   Matter.Runner.run(engine);
 }
 
 function draw() {
   background('black');
 
+  //Strecke und Böden draw
   startpunkt.draw();
   strecke01.draw();
   strecke02.draw();
+  bodenTest.draw();
+
+  //Ball und Maus draw
   ball.draw();
   mouse.draw();
+
+  //Hindernisse und Spielmechanismen draw
   stein01.draw();
   eis01.draw();
-   // follow the ball by scrolling the window
-scrollFollow(ball);
-
-//eis(ball);
-gameover(ball);
+  
+  //Spielmechanismus
+  scrollFollow(ball);
+  eis(ball);
+  gameover(ball);
 
 }
-
 
 //Kamera
 function scrollFollow(object) {
@@ -90,8 +96,7 @@ function scrollFollow(object) {
     const $element = $('html, body');
     if ($element.is(':animated') == false) {
       $element.animate({
-        scrollLeft: object.body.position.x,
-        scrollTop: object.body.position.y
+        scrollLeft: object.body.position.x - (1280 * 0.3) ,
       }, 720);
     }
   }
@@ -99,56 +104,48 @@ function scrollFollow(object) {
 
 function insideViewport(object) {
   const x = object.body.position.x;
-  const y = object.body.position.y;
   const pageXOffset = window.pageXOffset || document.documentElement.scrollLeft;
-  const pageYOffset  = window.pageYOffset || document.documentElement.scrollTop;
-  if (x >= pageXOffset && x <= pageXOffset + windowWidth &&
-      y >= pageYOffset && y <= pageYOffset + windowHeight) {
+  if (x >= pageXOffset && x <= pageXOffset + windowWidth * 0.6) {
     return true;
   } else {
     return false;
   }
 }
 
+//Löst aus wenn der Ball auf Eis ist
 function eis(object) {
   const x = object.body.position.x;
   const y = object.body.position.y;
-  if (x <= 1150) {
-    //object.body.position.x = object.body.position.x +1;
-  }
-  if (x <= 1150) {
-    //object.body.position.x = object.body.position.x +1;
+  const engine = Matter.Engine.create();
+
+  //eis01
+  if (x >= 900 && x < 1100 && y >=480) {
+    object.body.position.x = object.body.position.x +1;
+    engine.timing.timeScale = 1.5;
   }
   else 
   {
-
+    object.body.position.x = object.body.position.x;
+    engine.timing.timeScale = 1.0;
   }
 }
 
+//Löst das Game Over aus und resetet den Ball
 function gameover(object) {
   const y = object.body.position.y;
   if (y >= 720) {
     Matter.Body.setPosition(object.body, {x:30,y:50});
   }
-  else 
-  {
-
-  }
 }
 
-//Jump
+//Sprung
 function keyPressed() {
   // is SPACE pressed?
   if (keyCode === 32) {
-    let direction = 1; // ball runs left to right ->
-    //if ((ball.body.position.x - ball.body.positionPrev.x) < 0) {
-    //  direction = -1; // ball runs right to left <-
-    //}
-    // use current direction for the jump
     Matter.Body.applyForce(
       ball.body,
       {x: ball.body.position.x, y: ball.body.position.y},
-      {x: (0.01 * direction) , y: -0.07}
+      {x: 0.01 , y: -0.07}
     );
   }
 }
